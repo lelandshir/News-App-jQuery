e0f0061448127f42655ac4ab759af22ce2d3cfef
lelandshir <lelandshir@gmail.com>
2020-05-18 13:25:25 -0700

    routine commit

--- js/app.js
+++ js/app.js
@@ -5,16 +5,17 @@ const $userInput = $('input[type="text"]').val();
 // let link = `http://newsapi.org/v2/top-headlines?country=us&category=technology&apiKey=61a0acd611c1417e8f154193a705fd5c`;
 
 $(() => {
+  //clear buton --working on emptying only the populated html
   $(`#clear`).click((event) => {
     // console.log(`hi`);
     $(`#results`).children().empty(`<p>`);
   });
-
+  //on fetch click user searches for topic they type in=>topics are limited
   $(`#fetch`).click((ev) => {
     const $userInput = $(`#userInput`).val();
     let link = `http://newsapi.org/v2/top-headlines?country=us&category=${$userInput}&apiKey=61a0acd611c1417e8f154193a705fd5c`;
     event.preventDefault();
-
+    //ajax get request
     $.ajax({
       url: link,
       type: `GET`,
@@ -22,33 +23,29 @@ $(() => {
     })
       .then((data) => {
         console.log(data);
-        for (let i of data.articles) {
-          i++;
-          // for (let i = 0; i <= 20; i++) {
-          if ($userInput === " ") {
-            alert(`this field cannot be empty`);
-          } else {
-            //main results div
-            let $results = $(`#results`);
-            $(`#image`).html(`<img src="${data.articles[0].urlToImage}"/>`);
-            $(`#title`).html(data.articles[0].title);
-            $(`#author`).html(`${data.articles[0].author}`);
-            $(`#source`).html(data.articles[0].source.name);
-            $(`#link`).html(
-              `<a href="${data.articles[0].url}" target="_blank">tap here</a>`
-            );
-            $(`#description`).html(`${data.articles[0].description}`);
-            $(`#date`).html(`${data.articles[0].publishedAt}`);
 
-            // $(`#article`).html(data.articles[i].title);
+        for (let i of data.articles) {
+          for (let i = 0; i <= data.articles.length; i++) {
+            if ($userInput === " ") {
+              alert(`this field cannot be empty`);
+            } else {
+              let $results = $(`#results`);
+
+              $(`#title`).html(`${data.articles[0].title}`);
+              $(`#image`).html(`<img src="${data.articles[0].urlToImage}"/>`);
+              $(`#description`).html(`${data.articles[0].description}`);
+              $(`#link`).html(
+                `<a href="${data.articles[0].url}" target="_blank">tap here for the full story</a>`
+              );
+              $(`#author`).html(`${data.articles[0].author}`);
+              $(`#source`).html(`${data.articles[0].source.name}`);
+              $(`#date`).html(`${data.articles[0].publishedAt}`);
+            }
           }
         }
-        // }
       })
       .catch((error) => {
         console.log(error);
       });
   });
 });
-
-//honor agreement and add provider to bottom of page
