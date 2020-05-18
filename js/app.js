fbfaed462cfdea4a0a9bd1e3831a44598c889584
lelandshir <lelandshir@gmail.com>
2020-05-18 11:24:22 -0700

    fixed bug that repeated images and lins from articles

--- js/app.js
+++ js/app.js
@@ -5,7 +5,12 @@ const $userInput = $('input[type="text"]').val();
 // let link = `http://newsapi.org/v2/top-headlines?country=us&category=technology&apiKey=61a0acd611c1417e8f154193a705fd5c`;
 
 $(() => {
-  $(`.button`).click((ev) => {
+  $(`#clear`).click((event) => {
+    // console.log(`hi`);
+    $(`#results`).children().empty(`<p>`);
+  });
+
+  $(`#fetch`).click((ev) => {
     const $userInput = $(`#userInput`).val();
     let link = `http://newsapi.org/v2/top-headlines?country=us&category=${$userInput}&apiKey=61a0acd611c1417e8f154193a705fd5c`;
     event.preventDefault();
@@ -16,28 +21,29 @@ $(() => {
       limit: 20,
     })
       .then((data) => {
-        // console.log(data);
+        console.log(data);
         for (let i of data.articles) {
-          for (let i = 0; i <= 20; i++) {
-            if ($userInput === " ") {
-              alert(`this field cannot be empty`);
-            } else {
-              let $results = $(`#results`); //main results div
+          i++;
+          // for (let i = 0; i <= 20; i++) {
+          if ($userInput === " ") {
+            alert(`this field cannot be empty`);
+          } else {
+            //main results div
+            let $results = $(`#results`);
+            $(`#image`).html(`<img src="${data.articles[0].urlToImage}"/>`);
+            $(`#title`).html(data.articles[0].title);
+            $(`#author`).html(`${data.articles[0].author}`);
+            $(`#source`).html(data.articles[0].source.name);
+            $(`#link`).html(
+              `<a href="${data.articles[0].url}" target="_blank">tap here</a>`
+            );
+            $(`#description`).html(`${data.articles[0].description}`);
+            $(`#date`).html(`${data.articles[0].publishedAt}`);
 
-              $(`#image`).append(`<img src="${data.articles[0].urlToImage}"/>`);
-              $(`#title`).html(data.articles[0].title);
-              $(`#author`).html(`${data.articles[0].author}`);
-              $(`#source`).html(data.articles[0].source.name);
-              $(`#link`).append(
-                `<a href="${data.articles[0].url}" target="_blank">click here</a>`
-              );
-              $(`#description`).html(`${data.articles[0].description}`);
-              $(`#date`).html(`${data.articles[0].publishedAt}`);
-
-              // $(`#article`).html(data.articles[i].title);
-            }
+            // $(`#article`).html(data.articles[i].title);
           }
         }
+        // }
       })
       .catch((error) => {
         console.log(error);
