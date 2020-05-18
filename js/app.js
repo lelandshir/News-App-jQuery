f2abb0b8164b95be29465ee2cd1d6c96213dda15
lelandshir <lelandshir@gmail.com>
2020-05-17 19:30:29 -0700

    images and links to stories loading on fetch

--- js/app.js
+++ js/app.js
@@ -1,25 +1,41 @@
 // console.log(`linked!, ` + $);
 //query for #test button and have it return the value of the user #input from the form
-
+// const $userInput = $(`#input`).val();
+const $userInput = $('input[type="text"]').val();
 let link = `http://newsapi.org/v2/top-headlines?country=us&category=technology&apiKey=61a0acd611c1417e8f154193a705fd5c`;
 
 $(() => {
-  const $userInput = $(`#input`).val();
   $(`#test`).click((ev) => {
     event.preventDefault();
 
     $.ajax({
       url: link,
       type: `GET`,
+      limit: 20,
     })
       .then((data) => {
         // console.log(data);
-        if ($userInput === " ") {
-          alert(`this field cannot be empty`);
-        } else {
-          let $results = $(`#results`);
-          let $author = $(`#author`).append(`${data.articles[0].author}`);
-          // $results.append($author);
+        for (let i of data.articles) {
+          for (let i = 0; i < 20; i++) {
+            if ($userInput === " ") {
+              alert(`this field cannot be empty`);
+            } else {
+              console.log(data);
+
+              let $results = $(`#results`); //main results div
+
+              $(`#image`).append(`<img src="${data.articles[i].urlToImage}"/>`);
+              $(`#title`).html(data.articles[i].title);
+              $(`#author`).html(`${data.articles[i].author}`);
+              $(`#source`).html(data.articles[i].source.name);
+              $(`#link`).append(
+                `<a href="${data.articles[i].url}" target="_blank">click here</a>`
+              );
+              $(`#description`).html(`${data.articles[i].description}`);
+
+              // $(`#article`).html(data.articles[i].title);
+            }
+          }
         }
       })
       .catch((error) => {
@@ -27,3 +43,5 @@ $(() => {
       });
   });
 });
+
+//honor agreement and add provider to bottom of page
