73c2bfed20ff19b551bf83e0ffda2c7b4c7ad28e
lelandshir <lelandshir@gmail.com>
2020-05-21 10:44:05 -0700

    adjusted tag and input font sizing and heights

--- js/app.js
+++ js/app.js
@@ -17,7 +17,7 @@ $(() => {
     // $(`.input`).on(`keypress`, (ev) => {
     let userInput = $(`.input`).val();
 
-    let link = `https://newsapi.org/v2/top-headlines?country=us&category=${userInput}&apiKey=61a0acd611c1417e8f154193a705fd5c`;
+    let link = `https://cors-anywhere.herokuapp.com/https://newsapi.org/v2/top-headlines?country=us&category=${userInput}&apiKey=7cd19b7aa2bd403ba7770919b14157c2`;
 
     if ($(`.input`).val() != "") {
       $(`#container-2`).empty();
