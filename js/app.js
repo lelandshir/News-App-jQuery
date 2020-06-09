04d082cf5a8b220fb134a6602fb547180bb59901
lelandshir <lelandshir@gmail.com>
2020-06-09 13:08:01 -0400

    test2

--- js/app.js
+++ js/app.js
@@ -16,8 +16,8 @@ $(() => {
   $(`.button`).click((ev) => {
     // $(`.input`).on(`keypress`, (ev) => {
     let userInput = $(`.input`).val();
-
-    let link = `https://newsapi.org/v2/top-headlines?country=us&category=${userInput}&apiKey=7cd19b7aa2bd403ba7770919b14157c2`;
+    let key = cd19b7aa2bd403ba7770919b14157c2; //7 starts
+    let link = `https://newsapi.org/v2/top-headlines?country=us&category=${userInput}&apiKey=7${key}`;
 
     if ($(`.input`).val() != "") {
       $(`#container-2`).empty();
